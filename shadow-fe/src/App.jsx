d7a9737f27a4816afd6c0c599d48b36f9cc93831
PrashantGupta03 <prashant0gupta@nutanix.com>
2025-06-29 19:52:28 +0530

    Removed dead code/file

--- shadow-fe/src/App.jsx
+++ shadow-fe/src/App.jsx
@@ -10,45 +10,6 @@ import Register from "./containers/register";
 import RuleConfig from "./containers/ruleConfig";
 
 function App() {
-  const [response, setResponse] = useState(null);
-  const [fetchResponse, setFetchResponse] = useState(null);
-
-  const handlePost = async () => {
-    // try {
-    //   const res = await axios.post(
-    //     "https://jsonplaceholder.typicode.com/posts",
-    //     {
-    //       title: "Test Post",
-    //       body: "Hello from axios",
-    //       userId: 1,
-    //     }
-    //   );
-    //   setResponse(res.data);
-    //   console.log("Mocked response:", res.data);
-    // } catch (error) {
-    //   console.error("Request error:", error);
-    //   setResponse({ error: error.message });
-    // }
-
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title: "Test Post",
-        body: "Hello from axios",
-        userId: 1,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Fetch Response:", data);
-        setFetchResponse(data);
-      })
-      .catch((error) => console.error("Fetch Error:", error));
-  };
-
   return (
     <BrowserRouter>
       <AppHeader />
