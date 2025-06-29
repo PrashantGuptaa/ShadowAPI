2be1ac9f4f4ab1c1a59de99737280f63fad7f05e
PrashantGupta03 <prashant0gupta@nutanix.com>
2025-06-29 13:52:37 +0530

    Removed unused interceptorroutes

--- shadow-be/routes/index.js
+++ shadow-be/routes/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const interceptorRoutes = require("./interceptorRoutes");
 const userRoutes = require("./userRoutes");
 const ruleRoutes = require("./ruleRoutes");
 
-router.use("/interceptor", interceptorRoutes);
 router.use("/user", userRoutes);
 router.use("/rule", ruleRoutes);
 
